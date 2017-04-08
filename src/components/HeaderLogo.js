560d05557df3a6bbfcd9fa1920037482785784b8
Alon Nachshon <alonn@wix.com>
2017-04-08 12:28:51 +0300

    logo text shadow

--- src/components/HeaderLogo.js
+++ src/components/HeaderLogo.js
@@ -4,7 +4,8 @@ export default function HeaderLogo() {
     return (
         <div style={{
             color: 'rgb(176, 255, 249)',
-            fontSize: 18
+            fontSize: 18,
+            textShadow: '2px 1px rgba(0, 0, 0, 0.24)'
         }}>
             Resonators 
         </div>
