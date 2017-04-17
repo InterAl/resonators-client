a73d6c5581681af93c9ba5e2e37b9a7e503c72bc
Alon Nachshon <alonn@wix.com>
2017-04-17 19:03:34 +0300

    resonator feedback page: display in rtl for hebrew

--- src/components/ResonatorFeedback.js
+++ src/components/ResonatorFeedback.js
@@ -4,7 +4,9 @@ import { bindActionCreators } from 'redux';
 import {actions} from '../actions/feedbackActions';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import styles from './ResonatorFeedback.cssmodule.scss';
 import React, {Component} from 'react';
+import classNames from 'classnames';
 
 class ResonatorFeedback extends Component {
     constructor(props) {
@@ -13,6 +15,23 @@ class ResonatorFeedback extends Component {
         this.renderQuestion = this.renderQuestion.bind(this);
         this.renderAnswer = this.renderAnswer.bind(this);
         this.handleAnswerClick = this.handleAnswerClick.bind(this);
+    }
+
+    shouldDisplayRtl() {
+        function isHebrewLetter(letter) {
+            const code = letter.charCodeAt(0);
+            return code === 32 || code >= 1488 && code <= 1514;
+        }
+
+        const text = _.get(this.props, 'question.question.title', '');
+        const textArr = text.split('');
+
+        const hebrewLettersCount = _.reduce(textArr, (acc, letter) => {
+            return acc + (isHebrewLetter(letter) ? 1 : 0);
+        }, 0);
+
+        const isHebrew = hebrewLettersCount / textArr.length > 0.5;
+        return isHebrew;
     }
 
     handleAnswerClick(questionId, answerId) {
@@ -51,10 +70,17 @@ class ResonatorFeedback extends Component {
     renderQuestion(q) {
         const {question} = q;
         const answers = _.orderBy(question.answers, a => a.rank);
+        const rtl = this.shouldDisplayRtl();
 
         return (
             <Card key={q.id}>
                 <CardHeader
+                    className={
+                        classNames({
+                            [styles.resonatorTitleRtl]: rtl,
+                            [styles.resonatorTitleLtr]: !rtl
+                        })
+                    }
                     title={question.description}
                 />
                 <CardText>
@@ -78,7 +104,12 @@ class ResonatorFeedback extends Component {
         const {question} = this.props;
 
         return (
-            <div className='row' style={{'display': 'flex', width: '100%', marginTop: 30}}>
+            <div className='row' style={{
+                display: 'flex',
+                width: '100%',
+                marginTop: 30,
+                direction: this.shouldDisplayRtl() ? 'rtl' : 'ltr'
+            }}>
                 <div className='center-block'>
                     {question ? this.renderQuestion(question) :
                                 this.renderDone()}
---
+++ test/cases/resonatorsPage.test.js
@@ -0,0 +1,13 @@
+import createNightmare from '../nightmare';
+
+describe('resonators page', function() {
+    this.timeout(10000);
+
+    beforeEach(() => {
+    
+    });
+
+    it('create resonator', () => {
+        
+    });
+});
