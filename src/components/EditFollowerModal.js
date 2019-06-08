9c0ef1f9f9bedf3799e464e23b6c19df501c2d7d
purush3 <purush.pacholla@gmail.com>
2019-06-08 19:41:06 +0530

    updating default_clinic_id to current_clinic_id

--- src/components/EditFollowerModal.js
+++ src/components/EditFollowerModal.js
@@ -153,7 +153,7 @@ function mapStateToProps(state) {
     let {modalProps: {followerId, editMode}} = navigationInfoSelector(state);
     let follower = _.find(state.followers.followers, f => f.id === followerId);
     let clinics = state.clinics.clinics;
-    let default_clinic_id =  state.leaders.leaders.default_clinic_id;
+    let current_clinic_id =  state.leaders.leaders.current_clinic_id;
 
     let ret = {
         follower,
@@ -171,7 +171,7 @@ function mapStateToProps(state) {
     else
     {
         ret.initialValues = {
-            clinic: default_clinic_id
+            clinic: current_clinic_id
         };
     }
     return ret;
