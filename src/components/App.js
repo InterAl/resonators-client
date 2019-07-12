3cd9279f31a3b46125e91c60572e646d97b48d5a
ItamarGronich <itamargronich@gmail.com>
2019-07-12 21:18:53 +0300

    Add HomePage to routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { ConnectedRouter } from 'react-router-redux';
 import history from '../stores/history';
 import Layout from './Layout';
@@ -16,41 +16,45 @@ import CriteriaList from './CriteriaList';
 import CriteriaCreation from './CriteriaCreation/index';
 import ResetPasword from './ResetPassword';
 import ResonatorFeedback from './ResonatorFeedback';
+import HomePage from './HomePage';
 
-const {PropTypes} = React;
+const { PropTypes } = React;
 
 class AppComponent extends React.Component {
   static propTypes = {
-      loginInfo: PropTypes.object.isRequired,
-      navigationInfo: PropTypes.object.isRequired,
+    loginInfo: PropTypes.object.isRequired,
+    navigationInfo: PropTypes.object.isRequired,
   };
 
   render() {
-      return (
-          <ConnectedRouter history={history}>
-              <Route path='/'>
-                  <Layout>
-                      <Switch>
-                          <Route exact path="/followers/:followerId/resonators/new" component={EditResonator} />
-                          <Route exact path="/followers/:followerId/resonators/:resonatorId/edit" component={EditResonator} />
-                          <Route exact path="/followers/:followerId/resonators/:resonatorId/stats/:qid" component={ResonatorStats}/>
-                          <Route path="/followers/:followerId/resonators/:resonatorId" component={ShowResonator}/>
-                          <Route path="/followers/:followerId" component={FollowerResonators}/>
-                          <Route exact path="/followers" component={Followers} />
+    return (
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path='/(.+)'>
+            <Layout>
+              <Switch>
+                <Route exact path="/followers/:followerId/resonators/new" component={EditResonator}/>
+                <Route exact path="/followers/:followerId/resonators/:resonatorId/edit" component={EditResonator}/>
+                <Route exact path="/followers/:followerId/resonators/:resonatorId/stats/:qid" component={ResonatorStats}/>
+                <Route path="/followers/:followerId/resonators/:resonatorId" component={ShowResonator}/>
+                <Route path="/followers/:followerId" component={FollowerResonators}/>
+                <Route exact path="/followers" component={Followers}/>
 
-                          <Route exact path="/resetPassword" component={ResetPasword} />
-                          <Route exact path="*/criteria/submit" component={ResonatorFeedback} />
-                          <Route exact path="/clinics" component={Clinics}/>
-                          <Route exact path="/clinics/criteria/new" component={CriteriaCreation}/>
-                          <Route path="/clinics/criteria/:criterionId" component={CriteriaCreation}/>
-                          <Route exact path="/clinics/criteria" component={CriteriaList}/>
-                          <Route exact path="/login" component={Visitor} />
-                          <Route component={NoMatch} />
-                      </Switch>
-                  </Layout>
-              </Route>
-          </ConnectedRouter>
-      );
+                <Route exact path="/resetPassword" component={ResetPasword}/>
+                <Route exact path="*/criteria/submit" component={ResonatorFeedback}/>
+                <Route exact path="/clinics" component={Clinics}/>
+                <Route exact path="/clinics/criteria/new" component={CriteriaCreation}/>
+                <Route path="/clinics/criteria/:criterionId" component={CriteriaCreation}/>
+                <Route exact path="/clinics/criteria" component={CriteriaList}/>
+                <Route exact path="/login" component={Visitor}/>
+                <Route component={NoMatch}/>
+              </Switch>
+            </Layout>
+          </Route>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+      </ConnectedRouter>
+    );
   }
 }
 
