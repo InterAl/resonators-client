838c79aba403dad078212c99023dac2c673ec8e9
ItamarGronich <itamargronich@gmail.com>
2019-07-12 21:17:31 +0300

    Create HomePage component.

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="home-page">
+                Hello
+            </div>
+        );
+    }
+}
