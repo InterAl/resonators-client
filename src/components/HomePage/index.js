3ce1f7dc6b8815f4c97e44603033d386b3095ee9
ItamarGronich <itamargronich@gmail.com>
2019-07-13 19:36:06 +0300

    Add about section.

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './index.scss';
 import logoImg from "./assets/logo.svg";
-import bgImg from "./assets/bg.jpg";
-import bgImg2 from "./assets/bg@2x.jpg";
-import bgImg3 from "./assets/bg@3x.jpg";
+import illustrationImg from "./assets/group-33.png";
+import illustrationImg2 from "./assets/group-33@2x.png";
+import illustrationImg3 from "./assets/group-33@3x.png";
 
 export default class HomePage extends Component {
 
@@ -14,7 +14,7 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className="home-page">
-                <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400&display=swap" rel="stylesheet" />
                 <section className="fold">
 
                   <header className='header side-padding'>
@@ -42,7 +42,21 @@ export default class HomePage extends Component {
                       A secure cloud-based platform for enhancing therapy
                     </h1>
                     <p className="vertical-space">Created by therapists and developers for therapists and patients</p>
-                    <button className="btn-primary vertical-space-medium">Try it now</button>
+                    <a href="/login" className="btn-primary vertical-space-medium">Try it now</a>
+                  </article>
+                </section>
+
+                <section className="what-is-psysession">
+                  <article className="side-padding">
+                    <img className="what-is-psysession-image"
+                         src={illustrationImg}
+                         srcSet={`${illustrationImg2} 2x, ${illustrationImg3} 3x`}
+                         alt="psysession platform illustration"/>
+                    <h2 className="vertical-space">What is <br/> PsySession?</h2>
+                    <p className="vertical-space-medium">
+                      Breakthrough therapeutic sessions are exciting, but preserving their impact is difficult without continuous daily work. In most cases, the patient is likely to put aside important session content and forget about it until the next session. This impedes therapeutic progress.
+                    </p>
+                    <p><strong>This is where PsySession - a mobile and desktop cloud-based system - comes in.</strong></p>
                   </article>
                 </section>
             </div>
