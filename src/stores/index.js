880f9417fae1322d1fef7c82ab9d273a84f39292
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-07-24 20:02:24 +0300

    fixed redux logger breaking change

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,18 +2,17 @@ import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import history from './history';
 import createSagaMiddleware from 'redux-saga';
-import {sagas, reducers} from '../sagas';
-import createReduxLogger from 'redux-logger';
+import { sagas, reducers } from '../sagas';
+import logger from 'redux-logger';
 
 function reduxStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
-  const reduxLogger = createReduxLogger();
 
   const store = createStore(reducers, initialState,
     applyMiddleware(
-        sagaMiddleware,
-        reduxLogger,
-        routerMiddleware(history)
+      sagaMiddleware,
+      logger,
+      routerMiddleware(history)
     ));
 
   sagaMiddleware.run(sagas);
