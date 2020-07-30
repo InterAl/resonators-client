8438a9210a44cd20503116896e1a2c1593d26e7b
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-07-30 13:11:22 +0300

    turned resonator description field into textarea

--- src/components/EditResonator/Steps/basic.js
+++ src/components/EditResonator/Steps/basic.js
@@ -1,26 +1,22 @@
-import React, {Component} from 'react';
-import {Field} from 'redux-form';
-import TextField from '../../FormComponents/TextField';
-import StepBase from './stepBase';
+import React, { Component } from "react";
+import { Field } from "redux-form";
+import TextField from "../../FormComponents/TextField";
+import StepBase from "./stepBase";
 
 class EditResonatorBasic extends Component {
     render() {
         return (
             <div>
-                <Field name='title'
-                    placeholder='Title'
-                    type='text'
-                    component={TextField} />
-
-                <Field name='description'
-                    placeholder='Description'
-                    type='text'
-                    component={TextField} />
-
-                <Field name='link'
-                    placeholder='Link'
-                    type='text'
-                    component={TextField} />
+                <Field name="title" label="Title" component={TextField} />
+                <Field
+                    name="description"
+                    label="Description"
+                    multiline
+                    rows={2}
+                    rowsMax={10}
+                    component={TextField}
+                />
+                <Field name="link" label="Link" type="url" component={TextField} />
             </div>
         );
     }
@@ -30,13 +26,8 @@ export default StepBase({
     noBack: true,
     validate(formData) {
         let errors = {};
-
-        if (!formData.title)
-            errors.title = 'Required';
-
-        if (!formData.description)
-            errors.description = 'Required';
-
+        if (!formData.title) errors.title = "Required";
+        if (!formData.description) errors.description = "Required";
         return errors;
-    }
+    },
 })(EditResonatorBasic);
