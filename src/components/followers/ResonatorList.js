2dab58cd0cc47303fbc17ff78126c7bb768c624e
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-08-09 15:12:32 +0300

    finished resonators list

---
+++ src/components/followers/ResonatorList.js
@@ -0,0 +1,67 @@
+import React from "react";
+import moment from "moment";
+import {
+    List,
+    ListItem,
+    ListSubheader,
+    ListItemAvatar,
+    ListItemText,
+    Avatar,
+    Paper,
+    makeStyles,
+    Grow,
+    Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    itemBig: {
+        paddingTop: theme.spacing(2),
+        paddingBottom: theme.spacing(2),
+    },
+    avatarBig: {
+        height: theme.spacing(8),
+        width: theme.spacing(8),
+        marginRight: theme.spacing(2),
+    },
+    noResults: {
+        textAlign: "center",
+    },
+}));
+
+const formatResonatorTime = (time) => moment(time).format("dddd, MMMM Do YYYY, HH:mm");
+
+export default function ResonatorList({ resonators, subheader, paperProps = {}, big = false }) {
+    const classes = useStyles();
+    const listItemClass = big ? classes.itemBig : "";
+    const avatarClass = big ? classes.avatarBig : "";
+
+    return (
+        <List>
+            <ListSubheader>{subheader}</ListSubheader>
+            <Paper {...paperProps}>
+                {!resonators.length && (
+                    <ListItem className={classes.noResults}>
+                        <ListItemText primary={<Typography color="textSecondary">Nothing to show yet</Typography>} />
+                    </ListItem>
+                )}
+                {resonators.map((resonator) => (
+                    <Grow in key={resonator.id}>
+                        <ListItem button divider className={listItemClass}>
+                            {resonator.picture ? (
+                                <ListItemAvatar>
+                                    <Avatar src={resonator.picture} variant="rounded" className={avatarClass} />
+                                </ListItemAvatar>
+                            ) : null}
+                            <ListItemText
+                                inset={!resonator.picture}
+                                primary={resonator.title}
+                                secondary={formatResonatorTime(resonator.time)}
+                                primaryTypographyProps={{ noWrap: true }}
+                            />
+                        </ListItem>
+                    </Grow>
+                ))}
+            </Paper>
+        </List>
+    );
+}
--- src/components/followers/ResonatorsOverview.js
+++ src/components/followers/ResonatorsOverview.js
@@ -1,31 +1,55 @@
 import fetcher from "../../api/fetcher";
 import React, { useState, useEffect } from "react";
-import { List, ListItem, ListSubheader, ListItemAvatar, ListItemText, Avatar } from "@material-ui/core";
+import { Typography, makeStyles } from "@material-ui/core";
+
+import ResonatorList from "./ResonatorList";
+import LoadMoreButton from "./LoadMoreButton";
+
+const useStyles = makeStyles((theme) => ({
+    openResonatorsSubheader: {
+        marginBottom: theme.spacing(2),
+    },
+}));
 
 export default function ResonatorsOverview(props) {
     const [resonators, setResonators] = useState([]);
+    const [totalCount, setTotalCount] = useState(0);
     const [loading, setLoading] = useState(true);
+    const [page, setPages] = useState(0);
+
+    const classes = useStyles();
 
     useEffect(() => {
         setLoading(true);
-        fetcher("/follower/resonators")
-            .then((data) => data.resonators)
-            .then(setResonators)
+        fetcher(`/follower/resonators?page=${page}`)
+            .then((data) => {
+                setTotalCount(data.totalCount);
+                setResonators(resonators.concat(data.resonators));
+            })
             .then(() => setLoading(false));
-    });
+    }, [page]);
 
     return (
-        <List>
-            <ListSubheader>Your Resonators</ListSubheader>
-            {loading ? <div>Loading your resonators...</div> : null}
-            {resonators.map((resonator) => (
-                <ListItem key={resonator.id}>
-                    <ListItemAvatar>
-                        <Avatar src={resonator.thumbnail} />
-                    </ListItemAvatar>
-                    <ListItemText primary={resonator.title} secondary={resonator.content} />
-                </ListItem>
-            ))}
-        </List>
+        <>
+            {!resonators.length && loading ? <Typography variant="h6">Loading your resonators...</Typography> : null}
+            <ResonatorList
+                big
+                paperProps={{ elevation: 6 }}
+                resonators={resonators.filter((resonator) => !resonator.done)}
+                subheader={
+                    <Typography variant="h6" className={classes.openResonatorsSubheader}>
+                        Open Resonators
+                    </Typography>
+                }
+            />
+            <ResonatorList
+                paperProps={{ variant: "outlined" }}
+                resonators={resonators.filter((resonator) => resonator.done)}
+                subheader="Answered Resonators"
+            />
+            {resonators.length === totalCount ? null : (
+                <LoadMoreButton text="load older" loading={loading} loadMore={() => setPages(page + 1)} />
+            )}
+        </>
     );
 }
