2565a279b7198ee1c965928c7d804e867629f6b0
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-08-10 12:39:45 +0300

    turned layout to function component, removed unneeded withRouter, and made snackbars dense on small screens

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
+import React from "react";
 import { connect } from "react-redux";
-import React, { Component } from "react";
 import MomentUtils from "@date-io/moment";
-import { withRouter } from "react-router";
 import { SnackbarProvider } from "notistack";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { ThemeProvider, Toolbar, Grid } from "@material-ui/core";
@@ -10,41 +9,38 @@ import "./app.scss";
 import theme from "./theme";
 import TopBar from "./TopBar";
 import SideMenu from "./SideMenu";
+import { useBelowBreakpoint } from "./hooks";
 import ModalDisplayer from "./ModalDisplayer";
 import loginInfoSelector from "../selectors/loginInfo";
 import navigationSelector from "../selectors/navigationSelector";
 
-class Layout extends Component {
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
-                <SnackbarProvider>
-                    <MuiPickersUtilsProvider utils={MomentUtils}>
-                        <TopBar />
-                        <Toolbar />
-                        <Grid container wrap="nowrap">
-                            {this.props.loggedIn ? (
-                                <Grid item>
-                                    <SideMenu />
-                                </Grid>
-                            ) : null}
-                            <Grid item xs container justify="center" style={{ padding: 30 }}>
-                                <Grid item xs md={10} xl={8}>
-                                    {this.props.children}
-                                </Grid>
-                            </Grid>
+const Layout = (props) => (
+    <ThemeProvider theme={theme}>
+        <SnackbarProvider dense={useBelowBreakpoint("sm")}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+                <TopBar />
+                <Toolbar />
+                <Grid container wrap="nowrap">
+                    {props.loggedIn ? (
+                        <Grid item>
+                            <SideMenu />
                         </Grid>
-                        <ModalDisplayer modal={this.props.modal} />
-                    </MuiPickersUtilsProvider>
-                </SnackbarProvider>
-            </ThemeProvider>
-        );
-    }
-}
+                    ) : null}
+                    <Grid item xs container justify="center" style={{ padding: 30 }}>
+                        <Grid item xs md={10} xl={8}>
+                            {props.children}
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <ModalDisplayer modal={props.modal} />
+            </MuiPickersUtilsProvider>
+        </SnackbarProvider>
+    </ThemeProvider>
+);
 
 const mapStateToProps = (state) => ({
     modal: navigationSelector(state).modal,
     loggedIn: loginInfoSelector(state).loggedIn,
 });
 
-export default withRouter(connect(mapStateToProps, null)(Layout));
+export default connect(mapStateToProps, null)(Layout);
