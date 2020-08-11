eb5521411ee3a25feec08374a181ddb2ffd80de9
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-08-11 21:49:53 +0300

    redirect logged in followers to their resonators page

--- src/sagas/sessionSaga.js
+++ src/sagas/sessionSaga.js
@@ -141,7 +141,7 @@ function* updateUser(user = {}) {
         const currentPath = location.pathname;
 
         if (currentPath === '/' || currentPath === '/login')
-            yield put(navigationActions.navigate('followers'));
+            yield put(navigationActions.navigate(user.isFollower ? 'follower/resonators' : 'followers'));
     }
 
     return loggedIn;
