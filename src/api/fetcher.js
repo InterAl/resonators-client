8f27063d45260d02c43825993b5af0d45c18e0e5
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-06 00:44:40 +0300

    reformatted fetcher code

--- src/api/fetcher.js
+++ src/api/fetcher.js
@@ -1,79 +1,75 @@
-import cfg from 'config';
-import { saveAs } from 'file-saver';
-
+import cfg from "config";
+import { saveAs } from "file-saver";
 
 function fetcher(url, options = {}) {
-    const baseUrl = (cfg.baseUrl || '') + '/api';
+    const baseUrl = (cfg.baseUrl || "") + "/api";
     return fetch(`${baseUrl}${url}`, {
-        credentials: 'same-origin',
+        credentials: "same-origin",
         headers: getDefaultHeaders(),
-        ...options
+        ...options,
     })
         .then(async (response) => {
             if (response.status >= 300) {
                 if (response.status === 401)
                     return Promise.reject({
-                        unauthorized: true
+                        unauthorized: true,
                     });
-                else
-                    return Promise.reject(response);
+                else return Promise.reject(response);
             }
             if (options.download && response.ok) {
                 const blob = await response.blob();
                 saveAs(blob, getResponseFileName(response));
-            }
-            else if (!options.emptyResponse && response.status !== 204)
-                return response.json();
+            } else if (!options.emptyResponse && response.status !== 204) return response.json();
         })
-        .catch(err => {
-            console.error('failed fetching', err);
+        .catch((err) => {
+            console.error("failed fetching", err);
             throw err;
         });
 }
 
 fetcher.post = (url, body, emptyResponse = false) => {
     return fetcher(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
             ...getDefaultHeaders(),
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
-        emptyResponse
+        emptyResponse,
     });
 };
 
 fetcher.upload = (url, body) => {
     return fetcher(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
             ...getDefaultHeaders(),
-            Accept: 'application/json, text/plain, */*'
+            Accept: "application/json, text/plain, */*",
         },
-        body
+        body,
     });
 };
 
 fetcher.put = (url, body) => {
     return fetcher(url, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
             ...getDefaultHeaders(),
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
     });
 };
 
 fetcher.delete = (url, body = {}) => {
     return fetcher(url, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
             ...getDefaultHeaders(),
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         emptyResponse: true,
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
     });
 };
 
@@ -81,25 +77,25 @@ fetcher.download = (url) => {
     return fetcher(url, {
         headers: {
             ...getDefaultHeaders(),
-            Accept: 'text/csv',
+            Accept: "text/csv",
         },
         download: true,
     });
-}
+};
 
 function getDefaultHeaders() {
-    if (localStorage.getItem('auth_token') != null)
+    if (localStorage.getItem("auth_token") != null)
         return {
-            'Authorization': localStorage.getItem('auth_token')
+            Authorization: localStorage.getItem("auth_token"),
         };
 }
 
 function getResponseFileName(res) {
-    const disposition = res.headers.get('content-disposition');
-    if (disposition && disposition.indexOf('attachment') !== -1) {
+    const disposition = res.headers.get("content-disposition");
+    if (disposition && disposition.indexOf("attachment") !== -1) {
         const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
         const matches = filenameRegex.exec(disposition);
-        return matches?.[1]?.replace(/['"]/g, '');
+        return matches?.[1]?.replace(/['"]/g, "");
     }
 }
 
