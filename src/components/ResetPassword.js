980d1e449a8f6a26cb0ffdc5b79e944f3c3298a6
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-10-12 02:13:29 +0300

    put reset password page into new layout and made a few cosmetic improvements

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import { actions } from '../actions/sessionActions';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
-import TextBox from './FormComponents/TextBox';
-import { CircularProgress, Button, Card, CardHeader, CardContent } from '@material-ui/core';
+import { connect } from "react-redux";
+import { reduxForm } from "redux-form";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { CircularProgress, Button, Card, CardHeader, CardContent, CardActions, Typography } from "@material-ui/core";
 
+import TextBox from "./FormComponents/TextBox";
+import Layout from "./layouts/EmptyCenteredLayout";
+import { actions } from "../actions/sessionActions";
 
 class ResetPassword extends Component {
-    static propTypes = {
-
-    };
+    static propTypes = {};
 
     constructor(props) {
         super(props);
@@ -24,59 +23,64 @@ class ResetPassword extends Component {
 
     render() {
         return (
-            <div className='row' style={{ 'display': 'flex', width: '100%', marginTop: 30 }}>
-                <div className='center-block'>
-                    <Card>
-                        <CardHeader
-                            title='Reset password'
-                            subheader='Please enter your new password below' />
-                        <CardContent>
-                            <TextBox name='password' type='password' label="Password" />
-                            <Button
-                                color="primary"
-                                variant="contained"
-                                disabled={this.props.invalid}
-                                onClick={this.props.handleSubmit(this.handleSubmit)}
-                                style={{ marginTop: 30, marginBottom: 30 }}>
-                                Submit
-                            </Button>
-                            <br />
-                            {this.props.showSpinner && <CircularProgress size={30} thickness={3} />}
-                            {this.props.success && (
-                                <div>
-                                    Your password has been successfully reset.<br />
-                                    You will be redirected to the login page shortly.
-                                </div>
-                            )}
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
+            <Layout>
+                <Card>
+                    <CardHeader title="Reset password" subheader="Please enter your new password below" />
+                    <CardContent>
+                        <TextBox name="password" type="password" label="Password" />
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            color="primary"
+                            variant="contained"
+                            disabled={this.props.invalid}
+                            onClick={this.props.handleSubmit(this.handleSubmit)}
+                        >
+                            Submit
+                        </Button>
+                        {this.props.showSpinner && <CircularProgress size={30} thickness={3} />}
+                        {this.props.success && (
+                            <Typography>
+                                Your password has been successfully reset.
+                                <br />
+                                You will be redirected to the login page shortly.
+                            </Typography>
+                        )}
+                    </CardActions>
+                </Card>
+            </Layout>
         );
     }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        resetPassword: actions.resetPassword
-    }, dispatch);
+    return bindActionCreators(
+        {
+            resetPassword: actions.resetPassword,
+        },
+        dispatch
+    );
 }
 
 function mapStateToProps(state) {
     return {
         success: state.session.resetPasswordSuccessful,
-        showSpinner: state.session.resetPasswordSpinner
+        showSpinner: state.session.resetPasswordSpinner,
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
-    form: 'resetPassword',
-    validate(form) {
-        let errors = {};
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    reduxForm({
+        form: "resetPassword",
+        validate(form) {
+            let errors = {};
 
-        if (!form.password)
-            errors.password = 'Required';
+            if (!form.password) errors.password = "Required";
 
-        return errors;
-    }
-})(ResetPassword));
+            return errors;
+        },
+    })(ResetPassword)
+);
